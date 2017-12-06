4730b99994695e5667135fe160e1f46651a8222a
Pavol Dobrocka <pdb@whitestein.com>
2017-12-06 08:57:23 +0100

    Day 6

---
+++ day06/code_extra.js
@@ -0,0 +1,24 @@
+module.exports = input => {
+  let buffer = input.split(/\s/).map(Number);
+  let history = [];
+  let cycles = 0;
+  
+  
+  while (!history.includes(buffer.join())) {
+    history.push(buffer.join());
+    let max = Math.max(...buffer);
+    let idx = buffer.indexOf(max);
+    
+    let inc = (max / buffer.length) | 0;
+    let rem = max % buffer.length;
+    buffer[idx] = 0;
+    
+    for (let i = 0; i < buffer.length; i++) {
+      buffer[i] += ((i > idx && i <= idx + rem) || (i + buffer.length > idx && i + buffer.length <= idx + rem))? inc + 1: inc;
+    }
+    
+    cycles++;
+  }
+  
+  return history.length - history.indexOf(buffer.join());
+}
