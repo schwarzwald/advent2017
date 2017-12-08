ec3bcd930de7bbd05a85b2698ddef183a32c52a6
Pavol Dobrocka <pdb@whitestein.com>
2017-12-08 08:18:16 +0100

    Day 8

---
+++ day08/code.js
@@ -0,0 +1,20 @@
+module.exports = input => {
+  let instructions = input.split('\r\n').map(n => /(\w+)\s(\w+)\s(-?\d+)\sif\s(.*)/.exec(n));
+  let registers = new Map();
+
+  const test = (condition, registers) => {
+    let [, reg, type, value] = /(\w+)\s([!=><]+)\s(-?\d+)/.exec(condition);
+    return eval(`${registers.has(reg)? registers.get(reg): 0} ${type} ${value}`);
+  }
+  
+  instructions.forEach(instr => {
+    let [, reg, type, amount, condition] = instr;
+    
+    if (test(condition, registers)) {
+      let current = registers.has(reg)? registers.get(reg): 0;
+      registers.set(reg, current + (type == 'inc'? +amount: -amount));   
+    }
+  });
+
+  return Math.max(...registers.values());
+}
