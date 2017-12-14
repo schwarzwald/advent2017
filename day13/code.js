a7e459070cb3d44166189ebe287596fabc3cc44e
bibo <bibaso@gmail.com>
2017-12-14 22:22:00 +0100

    Small improvements

--- day13/code.js
+++ day13/code.js
@@ -1,19 +1,19 @@
 module.exports = input => {
-  let layers = new Map();
-  
+  let layers = [];
+
   input.split('\r\n').forEach(line => {
     let [, index, depth] = /(\d+): (\d+)/.exec(line);
-    layers.set(+index, +depth);
+    layers[+index] = +depth;
   });
-  
+
   let max = Math.max(...layers.keys());
   let severity = 0;
-  
+
   for (let i = 0; i <= max; i++) {
-    if (layers.has(i) && (i % (layers.get(i) * 2 - 2)) == 0) {
-      severity += i * layers.get(i);
+    if (layers[i] && (i % (layers[i] * 2 - 2)) == 0) {
+      severity += i * layers[i];
     }
   }
-  
+
   return severity;
 }
--- day13/code_extra.js
+++ day13/code_extra.js
@@ -1,25 +1,25 @@
 module.exports = input => {
-  let layers = new Map();
-  
+  let layers = [];
+
   input.split('\r\n').forEach(line => {
     let [, index, depth] = /(\d+): (\d+)/.exec(line);
-    layers.set(+index, +depth);
+    layers[+index] =  +depth;
   });
-  
+
   let max = Math.max(...layers.keys());
   let isCaught = false;
   let delay = 0;
-  
+
   do {
     isCaught = false;
     for (let i = 0; i <= max; i++) {
-      if (layers.has(i) && ((i + delay) % (layers.get(i) * 2 - 2)) == 0) {
+      if (layers[i] && ((i + delay) % (layers[i] * 2 - 2)) == 0) {
         isCaught = true;
         delay++;
         break;
       }
     }
   } while (isCaught);
-  
+
   return delay;
 }
