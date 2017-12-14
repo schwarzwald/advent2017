87717d1a9791acd5e165b17097bdf7f5afc59bb2
Pavol Dobrocka <pdb@whitestein.com>
2017-12-14 15:04:15 +0100

    Day 14

---
+++ day14/code.js
@@ -0,0 +1,19 @@
+const hash = require('../day10/code_extra');
+
+module.exports = input => {
+  let used = 0;
+  
+  for (let i = 0; i < 128; i++) {
+    let hashString = hash(`${input}-${i}`);
+    for (let j = 0; j < 8; j++) {
+      let hex = parseInt(hashString.substring(j * 4, (j + 1) * 4), 16);
+      for (let k = 0; k < 16; k++) {
+        if ((hex >> k) & 1) {
+          used++;
+        }
+      }
+    }
+  }
+  
+  return used;
+}
